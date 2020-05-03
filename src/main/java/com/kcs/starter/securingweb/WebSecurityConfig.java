30eca2fbb1ce2e7b77a5bbd8acf5d0add6a0251c
Kiran Shettar <kcshettar@gmail.com>
2020-05-03 01:56:24 -0400

    line breaks

--- src/main/java/com/kcs/starter/securingweb/WebSecurityConfig.java
+++ src/main/java/com/kcs/starter/securingweb/WebSecurityConfig.java
@@ -15,31 +15,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 	@Override
-    protected void configure(AuthenticationManagerBuilder auth) 
-      throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication()
-          .withUser("user")
-          .password(encoder.encode("password"))
-          .roles("USER");
-    }
-    
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/h2-console/**").antMatchers(HttpMethod.OPTIONS, "/**");
-    }
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("user").password(encoder.encode("password")).roles("USER");
+	}
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/h2-console/**").antMatchers(HttpMethod.OPTIONS, "/**");
+	}
 }
